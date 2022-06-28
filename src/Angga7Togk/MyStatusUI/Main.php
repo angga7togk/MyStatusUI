5897ccb9a9fcc07c3e09c72d017ece4ee0840d1b
angga7togk <90005225+angga7togk@users.noreply.github.com>
2022-06-28 01:48:15 -0700

    Update Main.php

--- src/Angga7Togk/MyStatusUI/Main.php
+++ src/Angga7Togk/MyStatusUI/Main.php
@@ -39,21 +39,20 @@ class Main extends PluginBase implements Listener{
     
     public function MyStatusUI($player){
         $form = new SimpleForm(function(Player $player, int $data = null){
-            if($data === null){
-	        $this->getConfig()->get("Status")["Button"]["Message"];
-                return true;
-            }
-	    if($data === 0){
-		$this->getConfig()->get("Status")["Button"]["Message"];
-                return true;
-            }
-            
-        });
-        $content = str_replace (["{money}", "{player}", "{rank}", "{online}", "{max_online}", "{ping}", "{tps}", "{x}", "{y}", "{z}", "{item_name}", "{item_meta}", "{item_id}"], [$this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->myMoney($player), $player->getName(), $this->getServer()->getPluginManager()->getPlugin("PurePerms")->getUserDataMgr()->getGroup($player)->getName(), count($this->getServer()->getOnlinePlayers()), $this->getServer()->getMaxPlayers(), $player->getNetworkSession()->getPing(), $this->getServer()->getTicksPerSecond(), intval($player->getPosition()->getX()), intval($player->getPosition()->getY()), intval($player->getPosition()->getZ()), $player->getInventory()->getItemInHand()->getName(), $player->getInventory()->getItemInHand()->getMeta(), $player->getInventory()->getItemInHand()->getId()], $this->getConfig()->get("Status")["SimpleForm"]["Content"]);
-        $form->setTitle($this->getConfig()->get("Status")["SimpleForm"]["Title"]);
-        $form->setContent($content);
-        $form->addButton($this->getConfig()->get("Status")["Button"]["Name"], 0, $this->getConfig()->get("Status")["Button"]["Image"]);
-        $form->sendToPlayer($player);
-        return $form;
+                if($data === null){
+	                $this->getConfig()->get("Status")["Button"]["Message"];
+                        return true;
+		}
+	        if($data === 0){
+		        $this->getConfig()->get("Status")["Button"]["Message"];
+                        return true;
+		}
+	});
+            $content = str_replace (["{money}", "{player}", "{rank}", "{online}", "{max_online}", "{ping}", "{tps}", "{x}", "{y}", "{z}", "{item_name}", "{item_meta}", "{item_id}"], [$this->getServer()->getPluginManager()->getPlugin("EconomyAPI")->myMoney($player), $player->getName(), $this->getServer()->getPluginManager()->getPlugin("PurePerms")->getUserDataMgr()->getGroup($player)->getName(), count($this->getServer()->getOnlinePlayers()), $this->getServer()->getMaxPlayers(), $player->getNetworkSession()->getPing(), $this->getServer()->getTicksPerSecond(), intval($player->getPosition()->getX()), intval($player->getPosition()->getY()), intval($player->getPosition()->getZ()), $player->getInventory()->getItemInHand()->getName(), $player->getInventory()->getItemInHand()->getMeta(), $player->getInventory()->getItemInHand()->getId()], $this->getConfig()->get("Status")["SimpleForm"]["Content"]);
+       	    $form->setTitle($this->getConfig()->get("Status")["SimpleForm"]["Title"]);
+            $form->setContent($content);
+            $form->addButton($this->getConfig()->get("Status")["Button"]["Name"], 0, $this->getConfig()->get("Status")["Button"]["Image"]);
+            $form->sendToPlayer($player);
+            return $form;
     }
 }
